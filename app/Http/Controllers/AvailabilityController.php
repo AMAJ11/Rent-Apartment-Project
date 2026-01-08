5075d368e119d0b580693e83e1505b549f7ca8a0
Marwan <marwannakli@gmail.com>
2026-01-08 12:30:28 +0300

    add fillable to notification model and fix a coulmn name in availability

--- app/Http/Controllers/AvailabilityController.php
+++ app/Http/Controllers/AvailabilityController.php
@@ -13,7 +13,7 @@ class AvailabilityController extends Controller
     //مشان اعرض الاوقات المحجوزة فيها الشقة للمستخدم
     //رسالة للفرونت لا تخلي اليوزر يققدر يحجز بالاوقات لبرجعلك ياها
     public function showAvailabilty(int $apartment_id) {
-        $bookings = Availability::where('aprtment_id',$apartment_id)->get();
+        $bookings = Availability::where('apartment_id',$apartment_id)->get();
         return response()->json($bookings,200);
     }
 }
